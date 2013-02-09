8d74ec0f8d319dca74f4ca7e484643fdebc77ab8
Brent Christensen <brent@eventespresso.com>
2013-02-09 14:28:10 -0800

    add doc comments and output buffering [touch:1955]

--- EE_Price_Modifier.class.php
+++ EE_Price_Modifier.class.php
@@ -26,8 +26,8 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
  */
-add_action( 'plugins_loaded', array( 'EE_Price_Modifier', 'instance' ));		
 register_activation_hook( __FILE__, array( 'EE_Price_Modifier', 'activate_price_modifier' ));
+add_action( 'plugins_loaded', array( 'EE_Price_Modifier', 'instance' ));		
 /**
  * ------------------------------------------------------------------------
  *
@@ -41,7 +41,10 @@ register_activation_hook( __FILE__, array( 'EE_Price_Modifier', 'activate_price_
  */
 class EE_Price_Modifier {
 	
-  // instance of the EE_Price_Modifier object
+	// was activation successfull?
+	private static $_installed = FALSE;
+	
+	// instance of the EE_Price_Modifier object
 	private static $_instance = NULL;
 	
 	// price_mod version
@@ -57,12 +60,12 @@ class EE_Price_Modifier {
 
 
 	/**
-	*		@singleton method used to instantiate class object
-	*		@access public
-	*		@return class instance
+	*	@singleton method used to instantiate class object
+	*	@access public
+	*	@return class instance
 	*/	
 	public static function instance ( ) {
-		if ( defined( 'EVENT_ESPRESSO_VERSION' )) {				
+		if ( defined( 'EVENT_ESPRESSO_VERSION' ) && self::$_installed ) {				
 			// check if class object is instantiated
 			if ( self::$_instance === NULL  or ! is_object( self::$_instance ) or ! is_a( self::$_instance, __CLASS__ )) {
 				self::$_instance = new self();
@@ -76,10 +79,10 @@ class EE_Price_Modifier {
 
 
 	/**
-	*		private constructor to prevent direct creation
-	*		@Constructor
-	*		@access private
-	*		@return void
+	*	private constructor to prevent direct creation
+	*	@Constructor
+	*	@access private
+	*	@return void
 	*/	
 	private function __construct() {
 
@@ -87,28 +90,32 @@ class EE_Price_Modifier {
 		define( 'PRICE_MOD_DIR_URL', plugin_dir_url( __FILE__ ) );	
 
 		// set PRICE_MOD active to TRUE
-		add_filter( 'filter_hook_espresso_price_mod_active', '__return_true' );		
-		
-			add_filter('filter_hook_espresso_parse_question_response_for_price', 'espresso_parse_admin_question_response_for_price', 10, 3);
-			add_filter('filter_hook_espresso_parse_form_value_for_price', 'espresso_parse_form_value_for_price', 10, 2);
-
+		add_filter( 'filter_hook_espresso_price_mod_active', '__return_true' );
 
 		if ( is_admin() ) {
 			//$this->_price_mod_admin();	
-			add_action( 'action_hook_espresso_new_question_price_mod_tr', array( $this, '_generate_form_inputs' ), 10, 2);
-			add_filter( 'filter_hook_espresso_parse_admin_question_response_for_price', '_parse_admin_question_response_for_price', 10, 2);
+			add_action( 'action_hook_espresso_new_question_price_mod_tr', array( $this, '_generate_form_inputs' ), 10, 2 );
+			add_filter( 'filter_hook_espresso_parse_question_response_for_price', array( $this, '_parse_admin_question_response_for_price' ), 10, 3 );
+			add_filter( 'filter_hook_espresso_parse_admin_question_response_for_price', array( $this, '_parse_admin_question_response_for_price' ), 10, 2 );
+			add_filter( 'filter_hook_espresso_parse_question_answer_for_price', array( $this, 'espresso_parse_question_answer_for_price' ), 10, 2 );
 		} else {
 			//$this->_price_mod_frontend();
-			add_filter('filter_hook_espresso_parse_question_answer_for_price', 'espresso_parse_question_answer_for_price', 10, 2);
+			add_filter( 'filter_hook_espresso_parse_form_value_for_price', 'espresso_parse_form_value_for_price', 10, 2 );
 		}	
 		
 	}
 
 
 
+
 	
-	
-	public static function _version() {
+	/**
+	*	returns EE_Price_Modifier version
+	* 
+	*	@access 		public
+	*	@return 		string
+	*/		
+	public static function version() {
 		return self::$_version;
 	}
 
@@ -116,15 +123,23 @@ class EE_Price_Modifier {
 
 
 
-	function _generate_form_inputs ( $values, $price_mod = 'N' ) {
+	/**
+	*	_generate_form_inputs
+	* 
+	*	@access 		public
+	*	@param 		array		$values
+	*	@param 		boolean	$price_mod
+	*	@return 		string
+	*/	
+	public function _generate_form_inputs ( $values, $price_mod = 'N' ) {
+		ob_start();
 		?>
-
 		<tr id="add-price-modifier">
 			<th> <label for="price_mod">
 					<?php _e('Modifies Event Price', 'event_espresso');?>
 				</label>
 			</th>
-			<td><?php echo select_input('price_mod', $values, $price_mod); ?> <span class="description">
+			<td><?php echo select_input( 'price_mod', $values, $price_mod ); ?> <span class="description">
 				<?php _e('If this is set to "Yes", then you can add price modifers to the answer options you entered above.', 'event_espresso');?>
 				<br />
 				<?php _e('Price modifiers will then be added (or subtracted if negative) from the ticket price for that event.', 'event_espresso');?>
@@ -134,15 +149,24 @@ class EE_Price_Modifier {
 				<?php _e('Eg. If the question was "Choice of Dinner Entrée", answer options might be: " steak|39.95, chicken|34.95, vegan|29.95 "', 'event_espresso'); ?>
 				</span></td>
 		</tr>
-	<?php
+		<?php
+		$content = ob_get_clean();
+		return $content;
 	}
 
 
 
 
 
-	function espresso_parse_admin_question_response_for_price( $value = '', $price_mod = 'N' ) {
-		echo $value;
+	/**
+	*	_parse_admin_question_response_for_price
+	* 
+	*	@access 		public
+	*	@param 		array		$value
+	*	@param 		boolean	$price_mod
+	*	@return 		string
+	*/	
+	public function _parse_admin_question_response_for_price( $value = '', $price_mod = 'N' ) {
 		if ( $price_mod == 'Y' ) {
 			global $org_options;
 			$values = explode( '|', $value );
@@ -158,7 +182,16 @@ class EE_Price_Modifier {
 
 
 
-	function espresso_parse_question_response_for_price( $value = '', $price_mod = 'N', $attendee_id = FALSE ) {
+	/**
+	*	_parse_admin_question_response_for_price
+	* 
+	*	@access 		public
+	*	@param 		array		$value
+	*	@param 		boolean	$price_mod
+	*	@param 		boolean	$price_mod
+	*	@return 		string
+	*/	
+	public function _parse_question_response_for_price( $value = '', $price_mod = 'N', $attendee_id = FALSE ) {
 		if ( $price_mod == 'Y' ) {
 			global $org_options;
 			$values = explode( '|', $value );
@@ -175,8 +208,7 @@ class EE_Price_Modifier {
 				$SQL = 'UPDATE '. EVENTS_ATTENDEE_TABLE .' SET final_price = final_price ' . $plus_or_minus . ' %f where id = %d';	
 				$wpdb->query( $wpdb->prepare( $SQL, $price, $attendee_id ));
 				//echo '<h4>LQ : ' . $wpdb->last_query . '  <br /><span style="font-size:10px;font-weight:normal;">' . __FILE__ . '<br />line no: ' . __LINE__ . '</span></h4>';
-			}
-			
+			}			
 		}
 		return $value;
 	}
@@ -185,7 +217,15 @@ class EE_Price_Modifier {
 
 
 
-	function espresso_parse_form_value_for_price( $value = '', $price_mod ) {
+	/**
+	*	_parse_form_value_for_price
+	* 
+	*	@access 		public
+	*	@param 		array		$value
+	*	@param 		boolean	$price_mod
+	*	@return 		string
+	*/	
+	public function _parse_form_value_for_price( $value = '', $price_mod ) {
 		if ( $price_mod == 'Y' ) {
 			global $org_options;
 			$values = explode( '|', $value );
@@ -200,7 +240,15 @@ class EE_Price_Modifier {
 
 
 
-	function espresso_parse_question_answer_for_price( $value = '', $price_mod = 'N' ) {
+	/**
+	*	_parse_question_answer_for_price
+	* 
+	*	@access 		public
+	*	@param 		array		$value
+	*	@param 		boolean	$price_mod
+	*	@return 		string
+	*/	
+	public function _parse_question_answer_for_price( $value = '', $price_mod = 'N' ) {
 		if ( $price_mod == 'Y' ) {
 			global $org_options;
 			$values = explode( '|', $value );
@@ -235,36 +283,40 @@ class EE_Price_Modifier {
 	*		activate EE_Price_Modifier
 	* 
 	*		@access 		public
-	*		@return 		array
+	*		@return 		void
 	*/	
 	public function activate_price_modifier() {
 
-		$mer_options = array();
-		$templates = array();
-		$templates['event_queue'] =  plugin_dir_path( __FILE__ ) . 'templates/event_queue.template.php';
-		$templates['view_event_queue_btn'] =  plugin_dir_path( __FILE__ ) . 'templates/view_event_queue_btn.template.php';	
-		$mer_options['templates'] = $templates;
-		update_option( 'mer_options', $mer_options ); 	
-		
-		$sql = "id int(11) unsigned NOT NULL auto_increment,
-				sequence INT(11) NOT NULL default '0',
-				question_type enum('TEXT','TEXTAREA','MULTIPLE','SINGLE','DROPDOWN') NOT NULL default 'TEXT',
-				question text NOT NULL,
-				system_name varchar(15) DEFAULT NULL,
-				response text NULL,
-				required ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
-				required_text text NULL,
-				price_mod ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
-				price_mod_limits text NULL,
-				price_mod_availalbe text NULL,
-				admin_only ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
-				wp_user int(22) DEFAULT '1',
-				PRIMARY KEY  (id),
-				KEY wp_user (wp_user),
-				KEY system_name (system_name),
-				KEY admin_only (admin_only)";
-		$sql = apply_filters( 'espresso_filter_hook_events_question_sql', $sql );	
-		
+
+//		$mer_options = array();
+//		$templates = array();
+//		$templates['event_queue'] =  plugin_dir_path( __FILE__ ) . 'templates/event_queue.template.php';
+//		$templates['view_event_queue_btn'] =  plugin_dir_path( __FILE__ ) . 'templates/view_event_queue_btn.template.php';	
+//		$mer_options['templates'] = $templates;
+//		update_option( 'mer_options', $mer_options ); 	
+		if ( file_exists( EVENT_ESPRESSO_PLUGINFULLPATH . 'includes/functions/database_install.php' )) {
+			require_once( EVENT_ESPRESSO_PLUGINFULLPATH . 'includes/functions/database_install.php' );		
+			$sql = "id int(11) unsigned NOT NULL auto_increment,
+					sequence INT(11) NOT NULL default '0',
+					question_type enum('TEXT','TEXTAREA','MULTIPLE','SINGLE','DROPDOWN') NOT NULL default 'TEXT',
+					question text NOT NULL,
+					system_name varchar(15) DEFAULT NULL,
+					response text NULL,
+					required ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
+					required_text text NULL,
+					price_mod ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
+					price_mod_limits text NULL,
+					price_mod_availalbe text NULL,
+					admin_only ENUM( 'Y', 'N' ) NOT NULL DEFAULT 'N',
+					wp_user int(22) DEFAULT '1',
+					PRIMARY KEY  (id),
+					KEY wp_user (wp_user),
+					KEY system_name (system_name),
+					KEY admin_only (admin_only)";
+			event_espresso_run_install( 'events_question', EVENT_ESPRESSO_VERSION, $sql );
+			self::$_installed = TRUE;
+		}
+
 	}
 	//register_activation_hook(__FILE__, 'espresso_activate_price_modifier' );
 
